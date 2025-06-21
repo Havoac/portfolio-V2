3949a9d13b5f08e8b5d987cb8083345fe010ea47
Havoac <shivanshsingh9670@gmail.com>
2025-06-21 23:22:45 +0530

    Skills Section Done

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,7 +8,7 @@ function About() {
     <>
       <section
         id="about"
-        className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
+        className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-10 md:mt-10 lg:mt-10"
       >
         <div className="flex flex-col-reverse md:flex-row justify-between items-center">
           {/* Left Side */}
