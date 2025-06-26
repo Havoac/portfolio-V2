39ef1241ea2f3aa275134bdb1e7444ab4ed63b8c
Havoac <shivanshsingh9670@gmail.com>
2025-06-26 23:39:18 +0530

    Education Section Done

--- src/constants.js
+++ src/constants.js
@@ -1,35 +1,23 @@
 // Skills Section Logo's
 import htmlLogo from "./assets/tech_logo/html.png";
 import cssLogo from "./assets/tech_logo/css.png";
-import sassLogo from "./assets/tech_logo/sass.png";
 import javascriptLogo from "./assets/tech_logo/javascript.png";
 import reactjsLogo from "./assets/tech_logo/reactjs.png";
-import angularLogo from "./assets/tech_logo/angular.png";
 import reduxLogo from "./assets/tech_logo/redux.png";
-import nextjsLogo from "./assets/tech_logo/nextjs.png";
 import tailwindcssLogo from "./assets/tech_logo/tailwindcss.png";
-import gsapLogo from "./assets/tech_logo/gsap.png";
 import materialuiLogo from "./assets/tech_logo/materialui.png";
 import bootstrapLogo from "./assets/tech_logo/bootstrap.png";
 import springbootLogo from "./assets/tech_logo/springboot.png";
-import nodejsLogo from "./assets/tech_logo/nodejs.png";
-import expressjsLogo from "./assets/tech_logo/express.png";
 import mysqlLogo from "./assets/tech_logo/mysql.png";
-import mongodbLogo from "./assets/tech_logo/mongodb.png";
 import firebaseLogo from "./assets/tech_logo/firebase.png";
 import cLogo from "./assets/tech_logo/c.png";
 import cppLogo from "./assets/tech_logo/cpp.png";
 import javaLogo from "./assets/tech_logo/java.png";
-import pythonLogo from "./assets/tech_logo/python.png";
-import typescriptLogo from "./assets/tech_logo/typescript.png";
 import gitLogo from "./assets/tech_logo/git.png";
 import githubLogo from "./assets/tech_logo/github.png";
 import vscodeLogo from "./assets/tech_logo/vscode.png";
 import postmanLogo from "./assets/tech_logo/postman.png";
-import mcLogo from "./assets/tech_logo/mc.png";
-import figmaLogo from "./assets/tech_logo/figma.png";
 import netlifyLogo from "./assets/tech_logo/netlify.png";
-import vercelLogo from "./assets/tech_logo/vercel.png";
 import postgreLogo from "./assets/tech_logo/postgre.png";
 import csharpLogo from "./assets/tech_logo/csharp.png";
 import hibernateLogo from "./assets/tech_logo/hibernate.png";
@@ -43,23 +31,14 @@ import curiousJrLogo from "./assets/company_logo/curiousJr.png";
 import highRadiusLogo from "./assets/company_logo/highRadius.png";
 
 // Education Section Logo's
-import glaLogo from "./assets/education_logo/gla_logo.png";
-import bsaLogo from "./assets/education_logo/bsa_logo.png";
-import vpsLogo from "./assets/education_logo/vps_logo.png";
+import kiitLogo from "./assets/education_logo/kiit.png";
+import mtsLogo from "./assets/education_logo/mts.png";
+import stJohnsLogo from "./assets/education_logo/stJohns.png";
 
 // Project Section Logo's
-import githubdetLogo from "./assets/work_logo/github_det.png";
 import scmLogo from "./assets/work_logo/scm.png";
 import imdbLogo from "./assets/work_logo/imdb.png";
 import invoiceLogo from "./assets/work_logo/invoiceProject.png";
-import csprepLogo from "./assets/work_logo/cs_prep.png";
-import movierecLogo from "./assets/work_logo/movie_rec.png";
-import taskremLogo from "./assets/work_logo/task_rem.png";
-import npmLogo from "./assets/work_logo/npm.png";
-import webverLogo from "./assets/work_logo/web_dig.png";
-import cmLogo from "./assets/work_logo/cm.png";
-import imagesearchLogo from "./assets/work_logo/image_search.png";
-import removebgLogo from "./assets/work_logo/remove_bg.png";
 
 export const SkillsInfo = [
   {
@@ -151,39 +130,30 @@ export const experiences = [
 export const education = [
   {
     id: 0,
-    img: glaLogo,
-    school: "GLA University, Mathura",
-    date: "Sept 2022 - July 2024",
-    grade: "7.81 CGPA",
-    desc: "I have completed my Master's degree (MCA) in Computer Applications from GLA University, Mathura. During my time at GLA, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
-    degree: "Master of Computer Applications - MCA",
+    img: kiitLogo,
+    school: "KIIT University, Bhubaneswar",
+    date: "July 2019 - June 2023",
+    grade: "8.96 CGPA",
+    desc: "I have completed my bachelor's degree in Computer Science and Engineering from KIIT University, Bhubaneswar. During my time at GLA, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at KIIT University has been instrumental in shaping my technical abilities and professional growth.",
+    degree: "Bachelor of Technology - BTech",
   },
   {
     id: 1,
-    img: bsaLogo,
-    school: "BSA College, Mathura",
-    date: "Sept 2018 - Aug 2021",
-    grade: "73.2%",
-    desc: "I completed my Bachelor's degree in Computer Science (B.Sc.) from BSA College, Mathura. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
-    degree: "Bachelor of Science - BSC (Computer Science)",
-  },
-  {
-    id: 2,
-    img: vpsLogo,
-    school: "Vatsalya Public School Govardhan, Mathura",
-    date: "Apr 2017 - March 2018",
-    grade: "78%",
-    desc: "I completed my class 12 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+    img: mtsLogo,
+    school: "MTS Public School, Mirzapur",
+    date: "July 2018 - June 2019",
+    grade: "80.8 %",
+    desc: "I completed my 12th grade from MTS Public School with a focus on Physics, Chemistry, and Mathematics, along with Computer Science as an elective. This combination strengthened my analytical thinking and introduced me to programming fundamentals, which laid the foundation for my interest in software development and problem-solving.",
     degree: "CBSE(XII) - PCM with Computer Science",
   },
   {
-    id: 3,
-    img: vpsLogo,
-    school: "Vatsalya Public School Govardhan, Mathura",
-    date: "Apr 2015 - March 2016",
-    grade: "87.5%",
-    desc: "I completed my class 10 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Science with Computer.",
-    degree: "CBSE(X), Science with Computer Application",
+    id: 2,
+    img: stJohnsLogo,
+    school: "St John's Co-Ed School, Prayagraj",
+    date: "July 2016 - June 2017",
+    grade: "79.6 %",
+    desc: "I completed my 10th grade from St. John’s Co-Ed School, where I opted for Computer Applications as an elective subject over Commerce and Hindi. This early exposure to computers sparked my curiosity in technology and laid the groundwork for my continued interest in programming and software development.",
+    degree: "ICSE(X), Science with Computer Application",
   },
 ];
 
