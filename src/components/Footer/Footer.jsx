5dfdc65ed5e5f390b4f72afc6067352d099d0661
Havoac <shivanshsingh9670@gmail.com>
2025-06-28 04:08:43 +0530

    Footer Section done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,70 @@
-import React from 'react'
+import React from "react";
+import { FaFacebook, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 function Footer() {
-    return <></>
+  const handleScroll = (sectionId) => {
+    const section = document.getElementById(sectionId);
+
+    if (section) section.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <footer className="text-white py-8 px-[12vw] md:px-[7vw] lg:px-[20vw]">
+      <div className="container mx-auto text-center">
+        {/* Name / Logo */}
+        <h2 className="text-xl font-semibold text-purple-500">
+          Shivansh Singh
+        </h2>
+
+        {/* Navigation Links - Responsive */}
+        <nav className="flex flex-wrap justify-center space-x-4 sm:space-x-6 mt-4">
+          {[
+            { name: "About", id: "about" },
+            { name: "Skills", id: "skills" },
+            { name: "Experience", id: "experience" },
+            { name: "Projects", id: "work" },
+            { name: "Education", id: "education" },
+          ].map((item, index) => (
+            <button
+              key={index}
+              onClick={() => handleScroll(item.id)}
+              className="hover:text-purple-500 text-sm sm:text-base my-1"
+            >
+              {item.name}
+            </button>
+          ))}
+        </nav>
+
+        {/* Social Media Icons - Responsive */}
+        <div className="flex flex-wrap justify-center space-x-4 mt-6">
+          {[
+            {
+              icon: <FaFacebook />,
+              link: "https://www.facebook.com/shivansh.singh.395891/",
+            },
+            {
+              icon: <FaLinkedin />,
+              link: "https://www.linkedin.com/in/shivansh961/",
+            },
+            {
+              icon: <FaInstagram />,
+              link: "https://www.instagram.com/shivanshsingh4/",
+            },
+          ].map((item, index) => (
+            <a
+              key={index}
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xl hover:text-purple-500 transition-transform transform hover:scale-110"
+            >
+              {item.icon}
+            </a>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
